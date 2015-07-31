ea63df8648f3f3f308879640293343f39102817a
Frank Lazzarini <flazzarini@gmail.com>
2015-07-31 12:38:28 +0200

    Replaced wget with urlib

--- blocklister/models.py
+++ blocklister/models.py
@@ -1,9 +1,12 @@
 import logging
 import re
+from urllib.error import HTTPError, URLError
+from urllib.request import Request, urlopen
+from gzip import GzipFile
+from io import BytesIO
 from os.path import join
 from datetime import datetime
 from os.path import getmtime, exists
-from subprocess import check_call, CalledProcessError, STDOUT
 
 from blocklister.exc import DownloadError
 
@@ -18,13 +21,14 @@ class Blocklist(object):
         "(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})$"
     )
     template = "firewall_addresslist.jinja2"
-    nogzip = False
+    gzip = False
 
     def __init__(self, store, filename=None):
         self.name = self.__class__.__name__.lower()
         self.store = store
         self.filename = filename if filename else join(self.name + '.txt')
         self.filepath = join(self.store, self.filename)
+        self.request = Request(self.source)
 
     def __repr__(self):
         return (
@@ -50,35 +54,43 @@ class Blocklist(object):
             return file_date
         raise IOError("File not found")
 
-    def get(self):
+    def get(self, request=None):
         try:
-            LOG.debug("Get List from {}".format(self.source))
-            devnull = open('/dev/null', 'w')
+            LOG.info(
+                "Downloading new version of list from {}"
+                .format(self.source)
+            )
+            if not request:
+                request = self.request
 
-            if self.nogzip:
-                cmd = (
-                    "wget -O - {} > {}"
-                    .format(self.source, self.filepath)
-                )
-            else:
-                cmd = (
-                    "wget -O - {} | gunzip > {}"
-                    .format(self.source, self.filepath)
-                )
-            check_call(cmd, shell=True, stdout=devnull, stderr=STDOUT)
-            LOG.info("List has been saved to {}".format(self.filepath))
-        except CalledProcessError as exc:
-            raise DownloadError(exc)
-        finally:
-            devnull.close()
+            with urlopen(request) as response:
+                raw_content = response.read()
+
+                if self.gzip:
+                    LOG.debug("Source file is gziped, unpack file first")
+                    buf = BytesIO(raw_content)
+                    data = GzipFile(fileobj=buf)
+                    raw_content = data.read().decode('utf-8')
+                    data.close()
+                    buf.close()
+
+                destination_file = join(self.store, self.filename)
+
+                with open(destination_file, 'w') as fileobj:
+                    fileobj.write(raw_content)
+                    LOG.debug("File written to {}".format(destination_file))
+
+                return raw_content
+        except (HTTPError, URLError) as exc:
+            raise DownloadError("Could not download source {}".format(exc))
+        except IOError as exc:
+            raise DownloadError("Could not write file to disk {}".format(exc))
 
     def get_ips(self):
         results = []
         with open(self.filepath, 'r') as f:
             for line in f:
-                print(line)
                 res = re.search(self.regex, line)
-                print(res)
 
                 if not res:
                     continue
@@ -96,122 +108,127 @@ class Blocklist(object):
 
 class Ads(Blocklist):
     source = "http://list.iblocklist.com/?list=bt_ads"
+    gzip = True
 
 
 class Spyware(Blocklist):
     source = "http://list.iblocklist.com/?list=bt_spyware"
+    gzip = True
 
 
 class Level1(Blocklist):
     source = "http://list.iblocklist.com/?list=ydxerpxkpcfqjaybcssw"
+    gzip = True
 
 
 class Level2(Blocklist):
     source = "http://list.iblocklist.com/?list=gyisgnzbhppbvsphucsw"
+    gzip = True
 
 
 class Level3(Blocklist):
     source = "http://list.iblocklist.com/?list=uwnukjqktoggdknzrhgh"
+    gzip = True
 
 
 class Edu(Blocklist):
     source = "http://list.iblocklist.com/?list=imlmncgrkbnacgcwfjvh"
+    gzip = True
 
 
 class Proxy(Blocklist):
     source = "http://list.iblocklist.com/?list=xoebmbyexwuiogmbyprb"
+    gzip = True
 
 
 class Badpeers(Blocklist):
     source = "http://list.iblocklist.com/?list=cwworuawihqvocglcoss"
+    gzip = True
 
 
 class Microsoft(Blocklist):
     source = "http://list.iblocklist.com/?list=xshktygkujudfnjfioro"
+    gzip = True
 
 
 class Spider(Blocklist):
     source = "http://list.iblocklist.com/?list=mcvxsnihddgutbjfbghy"
+    gzip = True
 
 
 class Hijacked(Blocklist):
     source = "http://list.iblocklist.com/?list=usrcshglbiilevmyfhse"
+    gzip = True
 
 
 class Dshield(Blocklist):
     source = "http://list.iblocklist.com/?list=xpbqleszmajjesnzddhv"
+    gzip = True
 
 
 class Malwaredomainlist(Blocklist):
     source = "http://www.malwaredomainlist.com/hostslist/ip.txt"
     regex = "^(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}).*$"
-    nogzip = True
 
 
 class Openbl(Blocklist):
     source = "https://www.openbl.org/lists/base.txt.gz"
     regex = "^(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}).*$"
+    gzip = True
 
 
 class Openbl_180(Blocklist):
     source = "https://www.openbl.org/lists/base_180days.txt.gz"
     regex = "^(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}).*$"
+    gzip = True
 
 
 class Openbl_360(Blocklist):
     source = "https://www.openbl.org/lists/base_360days.txt.gz"
     regex = "^(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}).*$"
+    gzip = True
 
 
 class Spamhausdrop(Blocklist):
     source = "https://www.spamhaus.org/drop/drop.txt"
     regex = "^(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\/\d{1,2})\s;\sSBL.*.*$"
-    nogzip = True
 
 
 class Spamhausedrop(Blocklist):
     source = "https://www.spamhaus.org/drop/edrop.txt"
     regex = "^(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\/\d{1,2})\s;\sSBL.*.*$"
-    nogzip = True
 
 
 class Blocklistde_All(Blocklist):
     source = "http://lists.blocklist.de/lists/all.txt"
     regex = "^(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})$"
-    nogzip = True
 
 
 class Blocklistde_Ssh(Blocklist):
     source = "http://lists.blocklist.de/lists/ssh.txt"
     regex = "^(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})$"
-    nogzip = True
 
 
 class Blocklistde_Mail(Blocklist):
     source = "http://lists.blocklist.de/lists/mail.txt"
     regex = "^(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})$"
-    nogzip = True
 
 
 class Blocklistde_Imap(Blocklist):
     source = "http://lists.blocklist.de/lists/imap.txt"
     regex = "^(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})$"
-    nogzip = True
 
 
 class Blocklistde_Apache(Blocklist):
     source = "http://lists.blocklist.de/lists/apache.txt"
     regex = "^(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})$"
-    nogzip = True
 
 
 class Blocklistde_Ftp(Blocklist):
     source = "http://lists.blocklist.de/lists/ftp.txt"
     regex = "^(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})$"
-    nogzip = True
 
 
 class Blocklistde_Strongips(Blocklist):
     source = "http://lists.blocklist.de/lists/strongips.txt"
     regex = "^(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})$"
-    nogzip = True
