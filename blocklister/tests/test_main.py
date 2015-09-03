f6a790b9ea64a854fc1bd6292c0eb5e0cecd8e54
Frank Lazzarini <flazzarini@gmail.com>
2015-09-03 21:36:30 +0200

    Unittest the webui

--- blocklister/tests/test_main.py
+++ blocklister/tests/test_main.py
@@ -1,74 +1,57 @@
 import unittest
-from blocklister.main import app, get_class
-from blocklister.models import (
-    Ads,
-    Spyware,
-    Level1,
-    Level2,
-    Level3,
-    Edu,
-    Proxy,
-    Badpeers,
-    Microsoft,
-    Spider,
-    Hijacked,
-    Dshield,
-    Malwaredomainlist,
-    Openbl,
-    Openbl_180,
-    Openbl_360,
-    Spamhausdrop,
-    Spamhausedrop,
-    Blocklistde_All,
-    Blocklistde_Ssh,
-    Blocklistde_Mail,
-    Blocklistde_Imap,
-    Blocklistde_Apache,
-    Blocklistde_Ftp,
-    Blocklistde_Strongips,
-)
-
-
-class TestHelpers(unittest.TestCase):
-    def setUp(self):
-        self.classes = [
-            Ads,
-            Spyware,
-            Level1,
-            Level2,
-            Level3,
-            Edu,
-            Proxy,
-            Badpeers,
-            Microsoft,
-            Spider,
-            Hijacked,
-            Dshield,
-            Malwaredomainlist,
-            Openbl,
-            Openbl_180,
-            Openbl_360,
-            Spamhausdrop,
-            Spamhausedrop,
-            Blocklistde_All,
-            Blocklistde_Ssh,
-            Blocklistde_Mail,
-            Blocklistde_Imap,
-            Blocklistde_Apache,
-            Blocklistde_Ftp,
-            Blocklistde_Strongips,
-        ]
-
-    def test_get_class(self):
-        for klass in self.classes:
-            result = get_class(klass.__name__.title())
-            self.assertEqual(result, klass)
-
-    def test_get_class_raises(self):
-        with self.assertRaises(ValueError):
-            get_class("NonExistingClass")
+from unittest.mock import MagicMock, patch, PropertyMock
+from datetime import datetime
+from blocklister.main import app
+from blocklister.models import Blocklist
 
 
 class TestMain(unittest.TestCase):
     def setUp(self):
         self.client = app.test_client()
+        self.classes = []
+        for subcls in Blocklist.__subclasses__():
+            self.classes.append(subcls)
+
+    def test_index(self):
+        result = self.client.get("/")
+        self.assertEqual(result.status_code, 200)
+        self.assertEqual(result.mimetype, "text/plain")
+
+    def test_index_contains_all_lists(self):
+        result = self.client.get("/")
+        for klass in self.classes:
+            self.assertIn(klass.__name__, result.get_data().decode('utf-8'))
+
+    def test_changelog(self):
+        result = self.client.get("/changelog")
+        self.assertEqual(result.status_code, 200)
+        self.assertEqual(result.mimetype, "text/plain")
+
+    @patch('blocklister.main.Blocklist')
+    def test_get_default_lists(self, bl_mock):
+        klass = MagicMock()
+        klass.__name__ = "test"
+        type(klass).last_saved = PropertyMock(return_value=datetime.now())
+        klass.get_ips.return_value = ['1.1.1.1']
+
+        bl_mock.get_class.return_value = klass
+
+        url = "/{}".format(klass.__name__.lower())
+        result = self.client.get(url)
+        self.assertEqual(result.status_code, 200)
+        self.assertEqual(result.mimetype, "text/plain")
+
+    @patch('blocklister.main.Blocklist')
+    def test_get_lists_listname(self, bl_mock):
+        klass = MagicMock()
+        klass.__name__ = "test"
+        type(klass).last_saved = PropertyMock(return_value=datetime.now())
+        klass.get_ips.return_value = ['1.1.1.1']
+
+        bl_mock.get_class.return_value = klass
+
+        url = "/{}".format(klass.__name__.lower())
+        result = self.client.get(url)
+        self.assertEqual(result.status_code, 200)
+        self.assertEqual(result.mimetype, "text/plain")
+        self.assertIn("Magicmock", result.get_data().decode('utf-8'))
