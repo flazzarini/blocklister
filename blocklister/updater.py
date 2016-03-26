5785fd2ef3e073b6ac589f136017259e4ac37d25
Frank Lazzarini <flazzarini@gmail.com>
2016-03-26 17:18:12 +0100

    Cleanup on updater

--- blocklister/updater.py
+++ blocklister/updater.py
@@ -3,21 +3,26 @@ import time
 from threading import Thread
 
 from blocklister.models import Blocklist
+from blocklister.config import Config
 
-store = '/tmp'
 LOG = logging.getLogger(__name__)
 
 
 class Updater(Thread):
-    def __init__(self):
+    def __init__(self, config=Config()):
         Thread.__init__(self)
+        self.config = config
+        self.store = self.config.get(
+            'blocklister', 'store', default="/tmp")
+        self.interval = self.config.get_int(
+            'blocklister', 'update_interval', default=120)
 
     def run(self):
         LOG.info("Start Blocklister-Updater")
 
         instances = []
         for subcls in Blocklist.__subclasses__():
-            instances.append(subcls(store))
+            instances.append(subcls(self.store))
 
         while True:
             for instance in instances:
@@ -26,9 +31,10 @@ class Updater(Thread):
                         "Updating Blocklister list {}".format(
                             instance.__class__.__name__))
                     instance.fetcher.update()
-            time.sleep(20)
+            time.sleep(self.interval)
 
-if __name__ == "__main__":
+
+def run():
     logging.basicConfig(level=logging.INFO)
     REQLOG = logging.getLogger("requests")
     URLLOG = logging.getLogger("urllib3")
@@ -36,3 +42,6 @@ if __name__ == "__main__":
     URLLOG.setLevel(logging.WARN)
     u = Updater()
     u.run()
+
+if __name__ == "__main__":
+    run()
