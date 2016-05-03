657024a1ed1c286a8b70b09b23680cb776ea50dc
Frank Lazzarini <flazzarini@gmail.com>
2016-05-03 22:31:04 +0200

    Cleanup of unittests for summarizer

--- blocklister/tests/test_summerizer.py
+++ blocklister/tests/test_summerizer.py
@@ -1,21 +1,33 @@
 import unittest
-from unittest.mock import patch
 from blocklister.summerizer import Summerizer
 
+
 class TestSummerizer(unittest.TestCase):
+    def test_range_of_one(self):
+        smr = Summerizer(
+            ['213.239.193.209-213.239.193.209'])
+        result = smr.summary()
+        expected = ['213.239.193.209']
+        self.assertCountEqual(result, expected)
 
-    def testRangeOfOne(self):
-        smr = Summerizer(['213.239.193.209-213.239.193.209'])
-        self.assertEqual(smr.summary(),['213.239.193.209'])
+    def test_ordinary_range(self):
+        smr = Summerizer(
+            ['213.221.87.72-213.221.87.79'])
+        result = smr.summary()
+        expected = ['213.221.87.72-213.221.87.79']
+        self.assertCountEqual(result, expected)
 
-    def testOridnaryRange(self):
-        smr = Summerizer(['213.221.87.72-213.221.87.79'])
-        self.assertEqual(smr.summary(),['213.221.87.72-213.221.87.79'])
+    def test_range_overflow(self):
+        smr = Summerizer(
+            ['72.32.242.248-72.32.243.255', '64.69.78.73'])
+        result = smr.summary()
+        expected = (
+            ['72.32.242.248-72.32.243.255', '64.69.78.73'])
+        self.assertCountEqual(result, expected)
 
-    def testRangeOverflow(self):
-        smr = Summerizer(['72.32.242.248-72.32.243.255','64.69.78.73'])
-        self.assertEqual(smr.summary(),['64.69.78.73','72.32.242.248-72.32.243.255'])
-
-    def testReversedOrder(self):
-        smr = Summerizer(['72.32.243.255-72.32.242.248'])
-        self.assertEqual(smr.summary(),['72.32.242.248-72.32.243.255'])
+    def test_reverse_order(self):
+        smr = Summerizer(
+            ['72.32.243.255-72.32.242.248'])
+        result = smr.summary()
+        expected = ['72.32.242.248-72.32.243.255']
+        self.assertCountEqual(result, expected)
